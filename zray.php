f591f1877bb3ef6349d46d2f38b3a259dded07d1
Jisse Reitsma <info@yireo.com>
2015-03-02 11:09:48 +0100

    Add new plugin-tracing and JPath tracing, plus improved output

--- zray.php
+++ zray.php
@@ -7,7 +7,7 @@
  * @copyright Copyright 2015
  * @license   Zend Server License
  * @link      https://www.yireo.com/software/joomla/zray
- * @version   0.1.0
+ * @version   0.2.0
  */
 class Joomla
 {
@@ -22,9 +22,19 @@ class Joomla
 	private $joomlaModules = array();
 
 	/**
+	 * Internal listing of Joomla plugins
+	 */
+	private $joomlaPlugins = array();
+
+	/**
 	 * Internal listing of Joomla events (triggered by JEventDispatcher)
 	 */
 	private $joomlaEvents = array();
+
+	/**
+	 * Internal listing of Joomla files (triggered by JPath::find)
+	 */
+    private $joomlaPathFiles = array();
 
 	/**
 	 * Method to bind ZRayExtension instance internally
@@ -57,7 +67,61 @@ class Joomla
 		$storage['request'] = $this->getRequest();
 		$storage['modules'] = $this->getModules();
 		$storage['events'] = $this->getEvents();
+        $storage['plugins'] = $this->getPlugins();
+        $storage['files'] = $this->getFiles();
 	}
+
+	/**
+	 * Method called after a specific plugin oject method has been called
+	 *
+	 * @param array $context
+	 * @param array $storage
+	 */
+    public function afterPluginObjectCall($context, &$storage)
+    {
+        $arguments = $context['functionArgs'];
+        if (!isset($arguments[0][0])) {
+            return false;
+        }
+
+        $plugin = $context['this'];
+        $pluginClass = get_class($plugin);
+        $methodName = $context['locals']['event'];
+        $this->joomlaPlugins[] = array(
+            'type' => 'Object-based observer',
+            'class' => $pluginClass,
+            'method' => $methodName,
+        );
+    }
+
+	/**
+	 * Method called after a specific plugin function method has been called
+	 *
+	 * @param array $context
+	 * @param array $storage
+	 */
+    public function afterPluginFunctionCall($context, &$storage)
+    {
+        $arguments = $context['functionArgs'];
+        if (!isset($arguments[0][0])) {
+            return false;
+        }
+
+        $object = $arguments[0][0];
+        if(is_object($object) && $object instanceof JPlugin) {
+            $pluginClass = get_class($object);
+        } elseif(is_string($object) && preg_match('/^plg/i', $object)) {
+            $pluginClass = $object;
+        } else {
+            return false;
+        }
+
+        $this->joomlaPlugins[] = array(
+            'type' => 'Function-based observer',
+            'class' => $pluginClass,
+            'method' => $arguments[0][1],
+        );
+    }
 
 	/**
 	 * Method called after a specific event has been dispatched
@@ -67,20 +131,48 @@ class Joomla
 	 */
 	public function afterEventTrigger($context, &$storage)
 	{
-		$arguments = $context['functionArgs'];
-		if (empty($arguments))
-		{
-			return;
-		}
+        $arguments = $context['functionArgs'];
+        if (empty($arguments)) {
+            return false;
+        }
 
-		$event = $arguments[0];
-		if (isset($this->joomlaEvents[$event]))
-		{
-			$this->joomlaEvents[$event]++;
-		} else
-		{
-			$this->joomlaEvents[$event] = 1;
-		}
+        $eventHash = md5(var_export($arguments,true));
+        $eventName = array_shift($arguments);
+
+        $argument1 = null;
+        $argument2 = null;
+        $argument3 = null;
+        if(isset($arguments[0])) {
+            $arguments = $arguments[0];
+            for($i = 1; $i <= 3; $i++) {
+                if(empty($arguments)) break;
+                $name = 'argument'.$i;
+                $$name = array_shift($arguments);
+            }
+
+            if(!empty($arguments)) {
+                $arguments = var_export($arguments, true);
+            } else {
+                $arguments = null;
+            }
+        } else {
+            $arguments = null;
+        }
+
+        if(!isset($this->joomlaEvents[$eventHash])) {
+            $this->joomlaEvents[$eventHash] = array(
+                'event' => $eventName,
+                'count' => 1,
+                'argument1' => $argument1,
+                'argument2' => $argument2,
+                'argument3' => $argument3,
+                'arguments' => $arguments,
+            );
+        } else {
+            $this->joomlaEvents[$eventHash]['count']++;
+        }
+
+        return true;
 	}
 
 	/**
@@ -113,6 +205,27 @@ class Joomla
 	}
 
 	/**
+	 * Method called after a specific file has been found through JPath::find()
+	 *
+	 * @param array $context
+	 * @param array $storage
+	 */
+    public function afterPathFind($context, &$storage)
+    {
+        $arguments = $context['functionArgs'];
+        if (empty($arguments)) {
+            return false;
+        }
+
+        $path = $context['returnValue'];
+        $joomlaPath = str_replace(JPATH_ROOT, '', $path);
+        $this->joomlaPathFiles[] = array(
+            'Relative Path' => $joomlaPath,
+            'Absolute Path' => $path,
+        );
+    }
+
+	/**
 	 * Method to return an array representing the current HTTP request
 	 *
 	 * @return array
@@ -132,27 +245,57 @@ class Joomla
 	}
 
 	/**
+	 * Method to return an array of all found files
+	 *
+	 * @return array
+	 */
+    private function getFiles()
+    {
+        $templateFiles = array();
+        foreach($this->joomlaPathFiles as $file) {
+            $templateFiles[] = $file;
+        }
+
+        return $templateFiles;
+    }
+
+	/**
 	 * Method to return an array representing all rendered modules
 	 *
 	 * @return array
 	 */
 	private function getModules()
 	{
-		$modules = array();
-		foreach ($this->joomlaModules as $joomlaModule)
-		{
-			$moduleKey = $joomlaModule['title'];
-			$moduleValue = array();
-			$moduleValue[] = $joomlaModule['module'];
-			$moduleValue[] = $joomlaModule['position'];
-			$moduleValue[] = 'ID ' . $joomlaModule['id'];
-			$moduleValue[] = (empty($joomlaModule['content'])) ? 'No content' : 'Content';
+        $modules = array();
+        foreach($this->joomlaModules as $joomlaModule) {
 
-			$modules[] = array('Key' => $moduleKey, 'Value' => implode(' | ', $moduleValue));
-		}
+            $module = array();
+            $module['Title'] = $joomlaModule['title'];
+            $module['Module'] = $joomlaModule['module'];
+            $module['Position'] = $joomlaModule['position'];
+            $module['ID'] = $joomlaModule['id'];
+            $module['Content'] = (empty($joomlaModule['content'])) ? 'No content' : $joomlaModule['content'];
 
-		return $modules;
+            $modules[] = $module;
+        }
+
+        return $modules;
 	}
+
+	/**
+	 * Method to return an array representing all loaded plugins
+	 *
+	 * @return array
+	 */
+    private function getPlugins()
+    {
+        $plugins = array();
+        foreach($this->joomlaPlugins as $joomlaPlugin) {
+            $plugins[] = $joomlaPlugin;
+        }
+
+        return $plugins;
+    }
 
 	/**
 	 * Method to return an array representing all triggered events
@@ -161,11 +304,17 @@ class Joomla
 	 */
 	private function getEvents()
 	{
-		$events = array();
-		foreach ($this->joomlaEvents as $joomlaEvent => $joomlaEventCount)
-		{
-			$events[] = array('Key' => $joomlaEvent, 'Value' => 'Occurances: ' . $joomlaEventCount);
-		}
+        $events = array();
+        foreach($this->joomlaEvents as $joomlaEvent) {
+            $events[] = array(
+                'Event' => $joomlaEvent['event'],
+                'Occurances' => $joomlaEvent['count'],
+                'Argument 1' => var_export($joomlaEvent['argument1'], true),
+                'Argument 2' => var_export($joomlaEvent['argument2'], true),
+                'Argument 3' => var_export($joomlaEvent['argument3'], true),
+                'Other arguments' => var_export($joomlaEvent['arguments'], true),
+            );
+        }
 
 		return $events;
 	}
@@ -182,7 +331,10 @@ $zrayJoomla->getZRay()->setMetadata(array(
 $zrayJoomla->getZRay()->setEnabledAfter('JFactory::getApplication');
 
 // Trace functions
-$zrayJoomla->getZRay()->traceFunction('JEventDispatcher::trigger', function () {}, array($zrayJoomla, 'afterEventTrigger'));
-$zrayJoomla->getZRay()->traceFunction('JModuleHelper::renderModule', function () {}, array($zrayJoomla, 'afterModuleRender'));
-$zrayJoomla->getZRay()->traceFunction('JDocument::render', function () {}, array($zrayJoomla, 'afterDocumentRender'));
-
+$zrayJoomla->getZRay()->traceFunction('JPath::find', function(){}, array($zrayJoomla, 'afterPathFind'));
+$zrayJoomla->getZRay()->traceFunction('JDispatcher::trigger', function(){}, array($zrayJoomla, 'afterEventTrigger'));
+$zrayJoomla->getZRay()->traceFunction('JEventDispatcher::trigger', function(){}, array($zrayJoomla, 'afterEventTrigger'));
+$zrayJoomla->getZRay()->traceFunction('call_user_func_array', function(){}, array($zrayJoomla, 'afterPluginFunctionCall'));
+$zrayJoomla->getZRay()->traceFunction('JEvent::update', function(){}, array($zrayJoomla, 'afterPluginObjectCall'));
+$zrayJoomla->getZRay()->traceFunction('JModuleHelper::renderModule', function(){}, array($zrayJoomla, 'afterModuleRender'));
+$zrayJoomla->getZRay()->traceFunction('JDocument::render', function(){}, array($zrayJoomla, 'afterDocumentRender'));
