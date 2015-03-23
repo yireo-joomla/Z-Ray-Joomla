564bf453e87f82cda15b12af7b6be4fbe50f21c4
root <root@yireo-dev.com>
2015-03-23 18:27:18 +0100

    Version increment

--- zray.php
+++ zray.php
@@ -7,7 +7,7 @@
  * @copyright Copyright 2015
  * @license   Zend Server License
  * @link      https://www.yireo.com/software/joomla/zray
- * @version   0.2.2
+ * @version   0.2.3
  */
 class Joomla
 {
